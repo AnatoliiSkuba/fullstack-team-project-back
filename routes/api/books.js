38d944a3ac0376c8396a06f395662975eec8b9ac
Skuba Anatoliy <photomir.in.ua@gmail.com>
2022-10-07 10:04:10 +0300

    s

--- routes/api/books.js
+++ routes/api/books.js
@@ -1,31 +1,15 @@
-const express = require("express");
-const { books: ctrl } = require("../../controllers");
-const {
-  validationBody,
-  ctrlWrapper,
-  authenticate,
-  checkProtocol,
-} = require("../../middlewares");
+const express = require('express');
+const { books: ctrl } = require('../../controllers');
+const { validationBody, ctrlWrapper, authenticate } = require("../../middlewares");
 const { joiSchema } = require("../../models/book");
 
-const router = express.Router();
+const router = express.Router()
 
-router.get("/", checkProtocol, authenticate, ctrlWrapper(ctrl.getAllBooks));
+router.get('/', authenticate, ctrlWrapper(ctrl.getAllBooks));
 
-router.post(
-  "/",
-  checkProtocol,
-  authenticate,
-  validationBody(joiSchema),
-  ctrlWrapper(ctrl.addBook)
-);
+router.post('/', authenticate, validationBody(joiSchema), ctrlWrapper(ctrl.addBook));
 
-router.put(
-  "/:bookId",
-  checkProtocol,
-  authenticate,
-  validationBody(joiSchema),
-  ctrlWrapper(ctrl.addBookReview)
-);
+router.put('/:bookId', authenticate, validationBody(joiSchema), ctrlWrapper(ctrl.addBookReview));
+
 
 module.exports = router;
